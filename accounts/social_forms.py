2485993bec75ac676e17b1e49404d04cb66d795a
Magictea <sherbakovdaniil6@gmail.com>
2023-02-08 08:58:37 +0000

    Поправлены шаблоны на проде

--- accounts/social_forms.py
+++ accounts/social_forms.py
@@ -1,15 +1,6 @@
 from allauth.socialaccount.forms import SignupForm
 
 class MyCustomSocialSignupForm(SignupForm):
-
-    def save(self, request):
-        # Ensure you call the parent class's save.
-        # .save() returns a User object.
-        user = super(MyCustomSocialSignupForm, self).save(request)
-        # Add your own processing here.
-        # You must return the original result.
-        return user
-
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         for field in self.fields:
